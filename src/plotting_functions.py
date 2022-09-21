a5f4de567450e49b145480c72f2bdeea47ecedb0
yuxuan <yuxuanneo@gmail.com>
2022-09-22 01:05:01 +0800

    first draft of eda completed

--- src/plotting_functions.py
+++ src/plotting_functions.py
@@ -8,7 +8,7 @@ def plot_value_counts(df, feature_x, fig_size = (11, 5)):
     output: 1 subplot- Single subplot showing the distribution of the feature_x in the dataset. This is simply a 
             truncated version of the earlier defined plot_cat_feature_2_features function.
     '''
-    f, ax = plt.subplots(1, 1, figsize = (11,5))
+    f, ax = plt.subplots(1, 1, figsize = fig_size)
 
     sns.countplot(x = feature_x, data = df, ax = ax)
 
@@ -30,6 +30,19 @@ def plot_value_counts(df, feature_x, fig_size = (11, 5)):
     ax.set(yticklabels=[])
     
     plt.show()
+
+def plot_cont_per_class(df, cols_to_plot, target, fig_size = (11, 5)):
+    if len(cols_to_plot) > 1: 
+        fig , axs = plt.subplots(len(cols_to_plot), 1, figsize= fig_size,sharex = True, sharey = False)
+        for i, col in enumerate(cols_to_plot):
+            sns.boxplot(data = df, x = target, y = col, ax = axs[i])
+            axs[i].grid()
+        plt.tight_layout()
+        
+    elif len(cols_to_plot) == 1:
+        sns.set(rc={'figure.figsize':fig_size})
+        sns.boxplot(data = df, x = target, y = cols_to_plot[0])
+    
 
 def numerical_histogram_1d(df, cols_to_plot, targetcol):
   '''
