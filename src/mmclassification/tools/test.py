bc80a90bff990318d089f9f96be9b2a42b53d335
yuxuan <yuxuanneo@gmail.com>
2022-09-27 16:58:31 +0800

    edit test script

--- src/mmclassification/tools/test.py
+++ src/mmclassification/tools/test.py
@@ -204,7 +204,7 @@ def model_inference(config_path="configs/resnet50_butterfly.py",
         outputs = multi_gpu_test(model, data_loader, args.tmpdir,
                                  args.gpu_collect)
 
-    # return outputs
+    return outputs
     
     rank, _ = get_dist_info()
     if rank == 0:
