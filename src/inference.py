01312a258e1d9eb3eb84e76f68255bfd73ee655e
yuxuan <yuxuanneo@gmail.com>
2022-09-30 23:40:25 +0800

    fix path issues

--- src/inference.py
+++ src/inference.py
@@ -1,11 +1,16 @@
 from src.mmclassification.mmcls.apis.inference import init_model, inference_model
 # import mmcv
 import pandas as pd
+from pathlib import Path
 
-def inference(test_df_path, config_file, checkpoint_file):
+def inference(test_df_path, config_file, checkpoint_file, 
+              data_path = "data/butterfly_mimics/image_holdouts"):
     # Specify the path to model config and checkpoint file
+    data_path = Path(data_path)
     config_file = config_file # 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
     checkpoint_file = checkpoint_file # 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+
+    data_directory/subfolder/(img_name + ".jpg")
 
     # build the model from a config file and a checkpoint file
     model = init_model(config_file, checkpoint_file, device='cuda:0')
@@ -14,8 +19,9 @@ def inference(test_df_path, config_file, checkpoint_file):
     test_df = pd.read_csv(test_df_path)
     images = test_df["image"].to_list()
     preds = []
-    for img in images:
-        pred = inference_model(model, img)
+    for img_name in images:
+        full_img_path = data_path/(img_name +  ".jpg")
+        pred = inference_model(model, full_img_path)
         preds.append(pred)
 
     return preds
