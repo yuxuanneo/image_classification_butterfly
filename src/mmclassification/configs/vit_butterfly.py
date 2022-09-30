1ff1c42096b30136d7230d675a2586d46f70eec3
yuxuan <yuxuanneo@gmail.com>
2022-10-01 00:48:12 +0800

    syntax error

--- src/mmclassification/configs/vit_butterfly.py
+++ src/mmclassification/configs/vit_butterfly.py
@@ -1,7 +1,7 @@
 _base_ = [
     '_base_\models\vit-large-p32.py', # inherit model structure from resnet
     '_base_/datasets/butterfly_dataset.py', 
-    '_base_/default_runtime.py'
+    '_base_/default_runtime.py']
     
 # model settings
 model = dict(
