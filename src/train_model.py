d9fe065c704ba045cb888b6ac3f1b8ae19234c7f
yuxuan <yuxuanneo@gmail.com>
2022-10-01 16:14:19 +0800

    rename vars

--- src/train_model.py
+++ src/train_model.py
@@ -72,18 +72,18 @@ def train_model(data_dir='data/processed_data',
     # exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
     
     
-    model_ft = models.resnet101(pretrained=True)
-    num_ftrs = model_ft.fc.in_features
+    model_conv = models.resnet101(pretrained=True)
+    num_ftrs = model_conv.fc.in_features
     # Here the size of each output sample is set to 2.
     # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-    model_ft.fc = nn.Linear(num_ftrs, len(class_names))
+    model_conv.fc = nn.Linear(num_ftrs, len(class_names))
 
-    model_ft = model_ft.to(device)
+    model_conv = model_conv.to(device)
 
     criterion = nn.CrossEntropyLoss()
 
     # Observe that all parameters are being optimized
-    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    optimizer_ft = optim.SGD(model_conv.parameters(), lr=0.001, momentum=0.9)
 
     # Decay LR by a factor of 0.1 every 7 epochs
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
